8995c2341240071eeb6c713c3e197f2bbe3305bb
Hunter Gayden <gayden.hunter@gmail.com>
2020-07-30 21:19:44 -0400

    Define middleware to serve static node packages

--- server.js
+++ server.js
@@ -9,14 +9,16 @@ const server = express();
 //  designated landing pages
 const pages = /\/?(Groceries|Meals|Recipes)/i;
 
+//  check request for designated landing page
 server.use((req, res, next) => {
-    //  if the request is for one of the designated landing pages, serve it
     if(req.path.search(pages) >= 0) {
         res.sendFile(`${req.path}.html`, {root: path.join(__dirname, 'app')});
         return;
     }
     next();
 });
+//  check request for node package
+server.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
 server.use(express.static(path.join(__dirname, 'app')));
 
 server.listen(port, host, () => {
